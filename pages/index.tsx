4e7603e1d0ae1407ba4cbce02efa73be7032217a
be1an <8497715@gmail.com>
2022-07-01 13:48:57 +0500

    tailwind added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>index</h1>
+      <h1 className='font-bold text-xl'>
+        index
+      </h1>
     </div>
   );
 };
