0084957ab1ae8bf7efcd14393a093f4aed60422b
be1an <8497715@gmail.com>
2022-09-10 13:27:30 +0500

    fix: buttons props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,33 +1,39 @@
-/* eslint-disable react/no-unknown-property */
+import { IButtonProps, IPartState } from '../interfaces/interface';
 
-import { IButtonProps } from '../interfaces/interface';
+const stateMapping = {
+  ACTIVE: { bg: "fill-green-50", symbol: "text-green-800", frame: "fill-stone-500"},
+  INACTIVE: { bg: "fill-zinc-100", symbol: "text-zinc-500", frame: "fill-stone-400"},
+  ERROR: { bg: "fill-red-300", symbol: "text-red-900", frame: "fill-stone-500"},
+  HIDE: { bg: "fill-zinc-100", symbol: "text-zinc-200", frame: "fill-stone-200"},
+};
 
+export const Button = ({ state: propState, symbol: propSymbol }: IButtonProps) => {
+  const state: IPartState = propState ?? IPartState.HIDE;
+  const symbol: string = propSymbol ?? ".";
 
-export const Button = ({ state, symbol }: IButtonProps) => {
-  console.log(state, symbol);
   return (
     <>
-      <div className="w-10 relative m-5">
-        <span className="absolute bottom-0 top-0 left-0 right-0 text-center align-text-bottom text-slate-500 text-3xl">A</span>
+      <div className="w-10 relative m-3">
+        <div className="absolute bottom-0 top-0 left-0 right-0 font-mono text-center align-text-bottom text-2xl">
+          <span className={stateMapping[state].symbol}>
+            {symbol}
+          </span>
+        </div>
 
         <svg
-          className=""
           viewBox="0 0 40 40"
-          // width="40"
-          // height="40"
         >
           <rect
             width="40"
             height="40"
-            fill="#EAE9E0"
-            fillOpacity=".8"
+            className={stateMapping[state].bg}
+            // fillOpacity=".8"
             rx="8"
           />
           <path
-            fill="#6D7163"
+            className={stateMapping[state].frame}
             fillRule="evenodd"
             d="M8 0a8 8 0 0 0-8 8v24a8 8 0 0 0 8 8h24a8 8 0 0 0 8-8V8a8 8 0 0 0-8-8H8Zm1 2a7 7 0 0 0-7 7v19a7 7 0 0 0 7 7h22a7 7 0 0 0 7-7V9a7 7 0 0 0-7-7H9Z"
-            clipRule="evenodd"
           />
         </svg>
       </div>
--- pages/test-button.jsx
+++ pages/test-button.jsx
@@ -5,6 +5,11 @@ const testButtonPage = () => (
   <div>
     <h1 className="font-bold text-xl p-3">Botton testing</h1>
     <hr />
+    <Button symbol="A" state={IPartState.ACTIVE}/>
+    <Button symbol="s" state={IPartState.ERROR}/>
+    <Button symbol="d" state={IPartState.INACTIVE}/>
+    <Button symbol="F" state={IPartState.HIDE}/>
+    <Button symbol="g" state={IPartState.ACTIVE}/>
     <Button />
   </div>
 );
