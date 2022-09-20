f1f0d1213499184d50699e255f9c89c9dd09bf20
be1an <8497715@gmail.com>
2022-09-21 00:35:02 +0500

    test: grid testing

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -4,8 +4,8 @@ import { Button } from "./Button";
 import { keyCodes } from "../shared/key-codes"
 
 export const Grid = () => {
-  const rowCl = "flex flex-row gap-1 h-10 bg-slate-200";
-  const keyCl = "bg-red-100 w-10"
+  const rowCl = "inline-flex flex-nowrap gap-1 bg-slate-200 overflow-clip";
+  const keyCl = "bg-red-100"
   const layout = {
     [IVirtualKey.KeyA]: "A",
     [IVirtualKey.KeyB]: "B",
@@ -14,70 +14,80 @@ export const Grid = () => {
     [IVirtualKey.KeyE]: "E",
   }
 
-  console.log('>>>>', JSON.stringify(keyCodes));
-
   return (<>
-    <div className="w-auto">
+    <div className="flex-col inline-flex">
       <div className={rowCl}>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button symbol="f" state={IPartState.ACTIVE} /></div>
-        <div className={keyCl}><Button symbol="g" state={IPartState.INACTIVE} /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
+        <div className={keyCl}><Button symbol="`" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="1" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="2" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="3" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="4" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="5" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="6" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="7" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="8" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="9" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="0" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="-" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="=" state={IPartState.INACTIVE} /></div>
         <div className="bg-zinc-300 w-16" />
       </div>
       <div className={rowCl}>
         <div className="bg-zinc-300 w-16" />
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
+        <div className={keyCl}><Button symbol="q" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="w" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="e" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="r" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="t" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="y" state={IPartState.ACTIVE} /></div>
+        <div className={keyCl}><Button symbol="u" state={IPartState.ERROR} /></div>
+        <div className={keyCl}><Button symbol="i" state={IPartState.HIDE} /></div>
+        <div className={keyCl}><Button symbol="o" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="p" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="[" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="]" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="\" state={IPartState.INACTIVE} /></div>
       </div>
       <div className={rowCl}>
         <div className="bg-zinc-300 w-19" />
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
+        <div className={keyCl}><Button symbol="a" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="s" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="d" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="f" state={IPartState.INACTIVE} isHome /></div>
+        <div className={keyCl}><Button symbol="g" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="h" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="j" state={IPartState.INACTIVE} isHome/></div>
+        <div className={keyCl}><Button symbol="k" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="l" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol=";" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="'" state={IPartState.INACTIVE} /></div>
         <div className="bg-zinc-300 w-18" />
       </div>
       <div className={rowCl}>
         <div className="bg-zinc-300 w-24" />
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
-        <div className={keyCl}><Button /></div>
+        <div className={keyCl}><Button symbol="z" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="x" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="c" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="v" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="b" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="n" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="m" state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="," state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="." state={IPartState.INACTIVE} /></div>
+        <div className={keyCl}><Button symbol="/" state={IPartState.INACTIVE} /></div>
         <div className="bg-zinc-300 w-24" />
+      </div>
+      <div className={rowCl}>
+        <div className={keyCl}><Button /></div>
+        <div className={keyCl}><Button /></div>
+        <div className={keyCl}><Button /></div>
+        <div className="bg-zinc-300 w-12" />
+        <div className="bg-zinc-300 w-56" />
+        <div className="bg-zinc-300 w-12" />
+        <div className={keyCl}><Button /></div>
+        <div className={keyCl}><Button /></div>
+        <div className={keyCl}><Button /></div>
+        <div className={keyCl}><Button /></div>
       </div>
     </div>
   </>);
