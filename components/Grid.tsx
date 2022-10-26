55f2fb9d0e802e2116a80ecd1a2bdc0a8572851d
be1an <8497715@gmail.com>
2022-10-27 00:57:44 +0500

    fix: keyboard store object

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,46 +1,56 @@
 import _ from "lodash";
 import { IPartState } from "../interfaces/interface";
 import { Button } from "./Button";
-import { IKbdLayout, IKbdLayoutItem, us } from "../data/kbdLayout";
+import { IKbdLayout, IKbdLayoutItem, kbdUsStandart } from "../data/kbdLayout";
 import { en, ILngLayout } from "../data/lngLayout";
 
 const gridClass = "flex-col inline-flex";
-const rowClass = "inline-flex flex-nowrap bg-slate-200 overflow-clip";
+const rowClass = "inline-flex flex-nowrap bg-slate-900 overflow-clip";
 const keyClass = "flex items-stretch text-2xl w-10 h-10";
 
+
 const renderGrid = (kbdLayout: IKbdLayout, lngLayout: ILngLayout) => {
+
+  // TODO: make function and move it out from the scope
   const lngIndex = _(lngLayout)
     .filter((e) => !e.shift)
     .keyBy((e) => e.kbdKey)
     .value();
 
+  // TODO: move it out from the scope
+  // TODO: make this without separate export interface
   const renderRow = (keys: IKbdLayoutItem[]) =>
-    keys.map(({ kbdKey, isHome, additionClass = "", buttonClass = "" }) => (
+    keys.map(({ keyId, isHome=false, css = "", buttonCss = "" }) => (
       <div
-        className={`${keyClass} ${additionClass}`}
-        key={kbdKey}
+        className={`${keyClass} ${css}`}
+        key={keyId}
       >
         <Button
-          symbol={lngIndex[kbdKey]?.symbol}
+          symbol={lngIndex[keyId]?.symbol}
           state={IPartState.INACTIVE}
           isHome={isHome}
-          additionClass={buttonClass}
+          additionClass={buttonCss}
         />
       </div>
     ));
 
-  const renderRows = (layout: IKbdLayout) =>
-    layout.map((rows, index) => (
+  // TODO: move it out from the scope
+  const renderRows = ({ layout }: IKbdLayout) =>
+    layout.map(({ rowId, css="", keys }) => (
       <div
-        className={rowClass}
-        // eslint-disable-next-line react/no-array-index-key
-        key={index}
+        className={`${rowClass} ${css}`}
+        key={rowId}
       >
-        {renderRow(rows)}
+        {renderRow(keys)}
       </div>
     ));
 
-  return <div className={gridClass}>{renderRows(kbdLayout)}</div>;
+  return (
+    <div
+      className={`${gridClass} ${kbdLayout.css}`}
+    >
+      {renderRows(kbdLayout)}
+    </div>);
 };
 
-export const Grid = () => renderGrid(us, en);
+export const Grid = () => renderGrid(kbdUsStandart, en);
