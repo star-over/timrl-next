ae9f310c88862a273887cc85b844a70fbcb528da
be1an <8497715@gmail.com>
2022-11-06 01:35:39 +0500

    Revert "add: finger zones and base key"
    This reverts commit a015cdddb3534b67371582aa6120fc933656f196.

--- components/KbdButton.tsx
+++ components/KbdButton.tsx
@@ -1,10 +1,10 @@
-import { IPartState, IFingersKeys } from "../interfaces/interface";
+import { IPartState, IFingersIds } from "../interfaces/interface";
 import { FC } from "react";
 import { cva, VariantProps } from "cva";
 
 export interface KbdButtonProps extends VariantProps<typeof Classes>, CommonProps { };
 interface CommonProps {
-  isHome?: boolean;
+  homie?: boolean;
   css?: string;
   children: string;
 };
@@ -19,26 +19,29 @@ const Classes = cva("flex flex-grow justify-center rounded-lg border-2",
         [IPartState.INVISIBLE]: "invisible",
       },
       finger: {
-        [IFingersKeys.L0]: "bg-slate-300",
-        [IFingersKeys.R0]: "bg-stone-300",
-        [IFingersKeys.L1]: "bg-amber-300",
-        [IFingersKeys.R1]: "bg-pink-300",
-        [IFingersKeys.L2]: "bg-lime-300",
-        [IFingersKeys.R2]: "bg-cyan-300",
-        [IFingersKeys.L3]: "bg-green-300",
-        [IFingersKeys.R3]: "bg-teal-300",
-        [IFingersKeys.L4]: "bg-violet-300",
-        [IFingersKeys.R4]: "bg-orange-300",
+        [IFingersIds.L0]: "bg-slate-200",
+        [IFingersIds.L4]: "bg-pink-200",
+        [IFingersIds.L3]: "bg-green-200",
+        [IFingersIds.L2]: "bg-yellow-200",
+        [IFingersIds.L1]: "bg-lime-200",
+        [IFingersIds.R1]: "bg-violet-200",
+        [IFingersIds.R2]: "bg-cyan-200",
+        [IFingersIds.R3]: "bg-orange-200",
+        [IFingersIds.R4]: "bg-blue-200",
+        [IFingersIds.R0]: "bg-stone-200",
+      },
+      base: {
+        true: "border-4 border-zinc-500",
       },
     },
     defaultVariants: { state: IPartState.INACTIVE },
   });
 
-export const KbdButton: FC<KbdButtonProps> = ({ state, finger, isHome, css, children }) => {
+export const KbdButton: FC<KbdButtonProps> = ({ state, finger, base, homie, css, children }) => {
   return (
-    <div className={Classes({ state, finger, class: css })}>
+    <div className={Classes({ state, finger, base, class: css })}>
       <span>{children}</span>
-      {isHome
+      {homie
         ? <div className="self-end fixed w-3 h-1.5 border-t-2 border-gray-400/50" />
         : null}
     </div>
